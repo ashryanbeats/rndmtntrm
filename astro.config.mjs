5cd26de3b1f05a71b7a8969678db670ae2cd9574
Ash <accounts@ashryan.io>
2025-04-25 13:29:09 -0400

    Domain fix.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://ashryanbeats.github.io",
-  base: "/rndmtntrm",
   outDir: "./dist",
   build: {
     // Generate static files
