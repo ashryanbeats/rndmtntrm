bdcdff36c297e96078734655e31c79f22ed86469
Ash <accounts@ashryan.io>
2025-04-26 21:57:55 -0400

    Domain resolution.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ashryanbeats.github.io",
+  site: "https://www.randomtantrum.com",
   outDir: "./dist",
 
   build: {
